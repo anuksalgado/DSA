b3c00ae789e412647b9dcb75a97d53062f4483d5
anuksalgado <118955816+anuksalgado@users.noreply.github.com>
2024-07-14 18:00:10 +0800

    Adding react and tailwind to react_files

---
+++ react_files/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-7xl text-blue-400'>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
